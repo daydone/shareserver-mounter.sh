a8c2775747b9795743ed33239b8c210893713499
daydone <44883786+daydone@users.noreply.github.com>
2023-05-01 13:12:53 -0700

    Update share-mounter.sh

--- share-mounter.sh
+++ share-mounter.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-#Mounts Windows share server using cfis
-echo 'Please specify a user:'
-read user
-user=$1
-echo 'Please specify the share path:'
-read server
-server=$2
+# Mounts Windows share server using CIFS
 
-#I want to add an option to check whether to run with webdav also
+# Prompt the user for the necessary information
+read -p "Please specify the username: " username
+read -p "Please specify the server address: " server
 
- 
+# Add an option to check whether to run with WebDAV also
+read -p "Do you want to run with WebDAV? [y/n]: " webdav_option
 
-echo 'If you typoed please select enter and rerun the script!'
-sudo mount -t cifs '$2' /shareserver/ -o user=$1
+# Define the mount point
+mount_point="/shareserver"
+
+# Check if the mount point exists and create it if necessary
+if [ ! -d "$mount_point" ]; then
+    sudo mkdir "$mount_point"
+fi
+
+# Construct the mount command with the appropriate options
+if [[ $webdav_option =~ ^[Yy]$ ]]; then
+    # Use WebDAV
+    mount_cmd="sudo mount -t davfs \"$server\" \"$mount_point\" -o username=$username"
+else
+    # Use CIFS
+    mount_cmd="sudo mount -t cifs \"$server\" \"$mount_point\" -o username=$username"
+fi
+
+# Mount the share
+echo "Mounting $server to $mount_point..."
+eval "$mount_cmd"
+
+# Check if the mount was successful
+if [ $? -eq 0 ]; then
+    echo "Mount successful!"
+else
+    echo "Mount failed."
+fi
